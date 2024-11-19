3ea3f5c0cb42d5395983cabaea637102ac3ce03c
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-11-19 13:47:28 +0100

    Add project files.

---
+++ libbenchlab/include/libbenchlab/api.h
@@ -0,0 +1,34 @@
+﻿﻿// <copyright file="api.h" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2024 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#pragma once
+
+
+#if (defined(_MSC_VER) && !defined(LIBBENCHLAB_STATIC))
+
+#if defined(LIBBENCHLAB_EXPORTS)
+#define LIBBENCHLAB_API __declspec(dllexport)
+#else /* defined(LIBBENCHLAB_EXPORTS) */
+#define LIBBENCHLAB_API __declspec(dllimport)
+#endif /* defined(LIBBENCHLAB_EXPORTS)*/
+
+#else /* (defined(_MSC_VER) && !defined(LIBBENCHLAB_STATIC)) */
+
+#define LIBBENCHLAB_API
+
+#endif /* (defined(_MSC_VER) && !defined(LIBBENCHLAB_STATIC)) */
+
+
+#if defined(LIBBENCHLAB_EXPOSE_TO_TESTING)
+#define LIBBENCHLAB_TEST_API LIBBENCHLAB_API
+#else /* defined(LIBBENCHLAB_EXPOSE_TO_TESTING) */
+#define LIBBENCHLAB_TEST_API
+#endif /* defined(LIBBENCHLAB_EXPOSE_TO_TESTING) */
+
+
+#if !defined(_WIN32)
+#include "libbenchlab/linuxsal.h"
+#endif /* !defined(_WIN32) */
