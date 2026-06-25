eb0d1523fa74abf1809f33aaa986612dab714964
Christoph <christoph.mueller@hotmail.de>
2026-06-25 14:14:40 +0200

    Fix header.

--- cclient/cclient.c
+++ cclient/cclient.c
@@ -1,5 +1,5 @@
 ﻿// <copyright file="cclient.c" company="Visualisierungsinstitut der Universität Stuttgart">
-// Copyright © 2024 - 2025 Visualisierungsinstitut der Universität Stuttgart.
+// Copyright © 2024 - 2026 Visualisierungsinstitut der Universität Stuttgart.
 // Licensed under the MIT licence. See LICENCE file for details.
 // </copyright>
 // <author>Christoph Müller</author>
